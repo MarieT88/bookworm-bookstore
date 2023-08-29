9caab20475b131a70239c5b79bfdbd79e853b306
Marie Tornetto <marietornetto@gmail.com>
2023-08-29 00:07:00 +0000

    clean up component

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -1,26 +1,24 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams,  } from "react-router-dom";
-import { fetchOrders, fetchProducts, fetchCart } from "../store";
+import { fetchOrders} from "../store";
 
 const Order = () => {
   const { orders, auth } = useSelector((state) => state);
   const { id } = useParams();
   const dispatch = useDispatch();
   
-    useEffect(() => {
+   useEffect(() => {
     dispatch(fetchOrders());
-    dispatch(fetchProducts());
-    dispatch(fetchCart());
   }, [auth]);
   
   const order = orders.find((order) => id === order.id);
   
-    if (!order) {
+  if (!order) {
     return <div>Loading...</div>; // Display a loading indicator
   }
 
-  // Check if lineItems are available in the order
+  // Check if lineItems are available, [] placeholder while async is happening
   const lineItems = order.lineItems || [];
 
   return (
